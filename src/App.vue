81b7515969d936c02bdb77332ec93751e84c5d0b
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2025-02-18 16:25:06 +0000

    testing

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <video v-if="videoUrl" :src="videoUrl" controls autoplay></video>
+  <p v-else>Loading...</p>
+  <div>
+    <input type="text" @keydown="handleKeyDown">
+  </div>
+</template>
+
+<script setup>
+  import axios from 'axios'
+  import { ref, onMounted } from 'vue'
+  const videoId = "f4df0165a54b486a9493145aed4a594c"
+  const videoUrl = ref('')
+
+  async function getVideoUrl(input) {
+  try {
+    const response = await axios.get(`http://localhost:3000/video?query=${input}`)
+    console.log(response.data)
+    videoUrl.value = response.data.video_url
+  } catch (error) {
+    console.error("Error fetching video:", error)
+  }
+}
+
+const handleKeyDown = async (e) => {
+  if (e.key === 'Enter') getVideoUrl(e.target.value)
+}
+</script>
+
+<style scoped>
+
+</style>
