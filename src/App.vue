2231d1638491d9d8c99ec9c0b17891c86e893341
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2025-02-19 09:57:01 +0000

    start conversation button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
-  <video v-if="videoUrl" :src="videoUrl" controls autoplay></video>
-  <p v-else>Loading...</p>
-  <div>
+  <button 
+    v-if="!sessionStarted"
+    @click="startConversation"
+  >
+    Start Conversation
+  </button>
+  <div v-else>
+    <!-- <video v-if="videoUrl" :src="videoUrl" controls autoplay></video> -->
+    <!-- <p v-else>Loading...</p> -->
     <input type="text" @keydown="handleKeyDown">
   </div>
 </template>
@@ -11,11 +17,15 @@
   import { ref, onMounted } from 'vue'
   const videoId = "f4df0165a54b486a9493145aed4a594c"
   const videoUrl = ref('')
+  const sessionStarted = ref(false)
+  const sessionId = ref('')
 
-  onMounted(async () => {
+  const startConversation = async () => {
     const result = await axios.get('http://localhost:3000/session')
+    sessionStarted.value = true
     console.log(result)
-  })
+    sessionId.value = result.data.session_id
+  }
 
   async function getVideoUrl(input) {
     console.log(input)
